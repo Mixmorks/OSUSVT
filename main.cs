23840169fcde2ec90fc1dc9680cc566983590e88
Uli Bartels <bartelja@onid.oregonstate.edu>
2014-01-15 17:46:07 -0800

    Create main.cs
    This will be our main entry point to the program. I've prefilled it with a little bit of C# code for reference.

---
+++ main.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace SolarCar{
+
+	class PortOpening{ /* This is a comment. */
+	
+		int number_of_ports;
+		double baud_rate; // All variables in C# must be declared within classes.
+		
+		public double number_to_baud(int){
+		
+			return number_of_ports * 0.3214;
+		
+		}
+	
+	}
+	
+	class MainRoutine{
+	
+		static void Main(){
+		
+			
+		
+		}
+	
+	}
+
+}
