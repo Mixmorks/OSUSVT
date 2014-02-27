41ca80c8bd342fd40225dbe42b0eea52c0b16612
Mixmorks <bartelja@onid.oregonstate.edu>
2014-02-26 17:44:50 -0800

    Added a GetPort functionality. Please try.

--- main/main/Form1.cs
+++ main/main/Form1.cs
@@ -6,14 +6,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace main
 {
     public partial class mainForm : Form
     {
 
-        GPS.GPSclass gps_data = new GPS.GPSclass();
-        
+        GPS.GPSclass gps_data = new GPS.GPSclass("gps_port", 9600);
+
         public mainForm()
         {
             InitializeComponent();
@@ -21,8 +22,9 @@ namespace main
 
         private void button1_Click(object sender, EventArgs e)
         {
-            gps_data.sat_count+=2;
-            textbox.Text = gps_data.sat_count.ToString();
+            textbox.Text = "Test.";
+            string[] valid_ports = SerialPort.GetPortNames(); //GetPortNames() checks the registry (i.e. HKEY_LOCAL_MACHINE\HARDWARE\DEVICEMAP\SERIALCOMM)
+            textbox.Text = String.Join(", ", valid_ports);    //For some reason this turns up blank on my laptop.
         }
 
         private void textbox_Click(object sender, EventArgs e)
--- main/main/GPS.cs
+++ main/main/GPS.cs
@@ -22,57 +22,59 @@ namespace GPS
             bool _NoS = false;
             bool _EoW = false;
             string _UTC = "N/A";     //Time at position of GPS.
+            SerialPort gps_port;
 
-            public GPSclass()
-            {
-                
-            }
+        public GPSclass(string gps_port_name, int gps_port_baud)
+        {
+            gps_port = new SerialPort();
+            gps_port.PortName = gps_port_name;
+            gps_port.BaudRate = gps_port_baud;
 
-            public float latitude
-            {
-                get{ return _latitude; }
-                set{ _latitude = value; }
-            }
 
-            public float longitude
-            {
-                get{ return _longitude; }
-                set{ _longitude = value; }
-            }
 
-            public int sat_count
-            {
-                get{ return _sat_count; }
-                set{ _sat_count = (int)value; }
-            }
+        }
 
-            public float velocity
-            {
-                get{ return _velocity; }
-                set{ _velocity = value; }
-            }
+        public float latitude
+        {
+            get { return _latitude; }
+            set { _latitude = value; }
+        }
 
-            public float altitude
-            {
-                get { return _altitude; }
-                set { _altitude = value; }
-            }
+        public float longitude
+        {
+            get { return _longitude; }
+            set { _longitude = value; }
+        }
 
-            public string bearing
-            {
-                get { return _bearing; }
-                set { _bearing = value; }
-            }
+        public int sat_count
+        {
+            get { return _sat_count; }
+            set { _sat_count = (int)value; }
+        }
 
-            public int quality
-            {
-                get { return _quality; }
-                set { _quality = value; }
-            }
+        public float velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
 
-            bool open_ports()
-            {
-                return false;
-            }
+        public float altitude
+        {
+            get { return _altitude; }
+            set { _altitude = value; }
+        }
+
+        public string bearing
+        {
+            get { return _bearing; }
+            set { _bearing = value; }
+        }
+
+        public int quality
+        {
+            get { return _quality; }
+            set { _quality = value; }
+        }
+
     }
 }
