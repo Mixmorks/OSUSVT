acebe80604c3ac4d3bf6d9ddbfa54b083fc9934e
Cody Ray Hoeft <crhoeft@gmail.com>
2014-04-01 14:28:04 -0700

    Program Now Creates SQL Database and User connection fails, also old SVTTELEMETRY files were deleted because they are not needed

--- main/main/SQL.cs
+++ main/main/SQL.cs
@@ -3,20 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
 namespace SQL
 {
     //Static Information for easy changes
     class connection
     {
-        //Need to know this to open a mysql prompt and initially configure the database
-        public const string pathMYSQL = "C:\\Program Files\\MySQL\\MySQL Server 5.6\\bin\\mysql.exe"; //Don't forget to escape your '\'s 
-
         public const string sqlUsername = "OSUSVT";
         public const string sqlPassword = "ManBearPig";
         public const string sqlDatabase = "TELEMETRY";
-        public const int sqlTimeout = 30;
-
     }
     class SQLclass
     {
@@ -31,7 +27,54 @@ namespace SQL
          */
         public SQLclass()
         {
-            svt_telemetry.Open();
+            try
+            {
+                svt_telemetry.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException exep)
+            {
+                if (exep.Number == 1042) //We Cant establish a connection because the server is not on or installed
+                {
+                    MessageBox.Show(exep.Message + "\nMySQL is probably not running or not installed");
+                } 
+                /*
+                 * Try to Automatically Create Database and a user
+                 */
+                if (exep.Number == 0) //Permissions Denied, we can attempt to fix this
+                {
+                    string sqlRoot = Microsoft.VisualBasic.Interaction.InputBox("In order to Please Enter MySQL root password: ", "Root Password", "OSUSVT");
+                    try
+                    {
+                        MySqlConnection setupdatabase = new MySqlConnection("uid=root; server=localhost; password=" + sqlRoot + ";");
+                        setupdatabase.Open();
+                        MySqlCommand adddata = new MySqlCommand("DROP USER '" + connection.sqlUsername + "'; ", setupdatabase);
+                        adddata.ExecuteNonQuery();
+                    }
+                    catch (MySql.Data.MySqlClient.MySqlException ex)
+                    {
+                        ;//If there is a probelm droping the user, we don't care about it, it probably did not exist
+                    }
+                    try
+                    {
+                        MySqlConnection setupdatabase = new MySqlConnection("uid=root; server=localhost; password=" + sqlRoot + ";");
+                        setupdatabase.Open();
+                        string CommandText = "CREATE DATABASE IF NOT EXISTS " + connection.sqlDatabase + "; " +
+                            "CREATE USER '" + connection.sqlUsername + "'@'%' IDENTIFIED BY '" + connection.sqlPassword + "'; " +
+                            "GRANT ALL PRIVILEGES ON " + connection.sqlDatabase + ".* TO '" + connection.sqlUsername + "'@'%'; ";
+
+                        MySqlCommand adddata = new MySqlCommand(CommandText, setupdatabase);
+                        adddata.ExecuteNonQuery();
+
+                    }
+                    catch (MySql.Data.MySqlClient.MySqlException ex)
+                    {
+                        MessageBox.Show("Failed to Configure the Database Automatically, try configuring manually or check the root password.\n" + "ERROR TEXT: " + ex.Message);
+                        //Program should quit here?
+                    }
+                    
+                }
+            }
+
         }
 
         public void update_database(string date, string utc, string longitude, string latitude, string velocity, string altitude)
