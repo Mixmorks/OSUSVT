f69102def8ef3e6a2fd147a95bf5b5d056d4bc66
Cody Ray Hoeft <crhoeft@gmail.com>
2014-04-19 11:04:01 -0700

    Reconfigured to a createdatabse(bool) function

--- main/main/SQL.cs
+++ main/main/SQL.cs
@@ -14,6 +14,7 @@ namespace SQL
         public const string sql_username = "OSUSVT";
         public const string sql_password = "ManBearPig";
         public const string sql_database = "TELEMETRY";
+        public static string sql_root = ""; //Set if cannot be configured
     }
     class SQLclass
     {
@@ -26,6 +27,48 @@ namespace SQL
         /*
         SqlCommand add_table = new SqlCommand("",svt_telemetry);
          */
+        public static void configuredatabase(bool displayerrors) 
+        {
+            if(displayerrors)
+                connection.sql_root = Microsoft.VisualBasic.Interaction.InputBox("In order to configure MySQL, Please Enter MySQL root password: ", "Root Password", "OSUSVT");
+            else if(connection.sql_root == "")
+                return;
+            try
+            {
+                MySqlConnection setupdatabase = new MySqlConnection("uid=root; server=localhost; password=" + connection.sql_root + ";");
+                setupdatabase.Open();
+                MySqlCommand adddata = new MySqlCommand("DROP USER '" + connection.sql_username + "'; ", setupdatabase);
+                adddata.ExecuteNonQuery();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException)
+            {
+                ;//If there is a probelm droping the user, we don't care about it, it probably did not exist
+            }
+            try
+            {
+                MySqlConnection setupdatabase = new MySqlConnection("uid=root; server=localhost; password=" + connection.sql_root + ";");
+                setupdatabase.Open();
+                string CommandText = "CREATE DATABASE IF NOT EXISTS " + connection.sql_database + "; " +
+                    "CREATE USER '" + connection.sql_username + "'@'%' IDENTIFIED BY '" + connection.sql_password + "'; " +
+                    "GRANT ALL PRIVILEGES ON " + connection.sql_database + ".* TO '" + connection.sql_username + "'@'%'; ";
+
+
+
+
+                MySqlCommand adddata = new MySqlCommand(CommandText, setupdatabase);
+                adddata.ExecuteNonQuery();
+
+                //Last Step, Open it up...
+                svt_telemetry.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                if(displayerrors)
+                    MessageBox.Show("Failed to Configure the Database Automatically, try configuring manually or check the root password.\n" + "ERROR TEXT: " + ex.Message);
+                //Program should quit here?
+            }
+                    
+        }
         public SQLclass()
         {
             try
@@ -43,45 +86,21 @@ namespace SQL
                  */
                 if (exep.Number == 0) //Permissions Denied, we can attempt to fix this
                 {
-                    string sqlRoot = Microsoft.VisualBasic.Interaction.InputBox("In order to configure MySQL, Please Enter MySQL root password: ", "Root Password", "OSUSVT");
-                    try
-                    {
-                        MySqlConnection setupdatabase = new MySqlConnection("uid=root; server=localhost; password=" + sqlRoot + ";");
-                        setupdatabase.Open();
-                        MySqlCommand adddata = new MySqlCommand("DROP USER '" + connection.sql_username + "'; ", setupdatabase);
-                        adddata.ExecuteNonQuery();
-                    }
-                    catch (MySql.Data.MySqlClient.MySqlException)
-                    {
-                        ;//If there is a probelm droping the user, we don't care about it, it probably did not exist
-                    }
-                    try
-                    {
-                        MySqlConnection setupdatabase = new MySqlConnection("uid=root; server=localhost; password=" + sqlRoot + ";");
-                        setupdatabase.Open();
-                        string CommandText = "CREATE DATABASE IF NOT EXISTS " + connection.sql_database + "; " +
-                            "CREATE USER '" + connection.sql_username + "'@'%' IDENTIFIED BY '" + connection.sql_password + "'; " +
-                            "GRANT ALL PRIVILEGES ON " + connection.sql_database + ".* TO '" + connection.sql_username + "'@'%'; ";
-
-                        MySqlCommand adddata = new MySqlCommand(CommandText, setupdatabase);
-                        adddata.ExecuteNonQuery();
-
-                        //Last Step, Open it up...
-                        svt_telemetry.Open();
-                    }
-                    catch (MySql.Data.MySqlClient.MySqlException ex)
-                    {
-                        MessageBox.Show("Failed to Configure the Database Automatically, try configuring manually or check the root password.\n" + "ERROR TEXT: " + ex.Message);
-                        //Program should quit here?
-                    }
-                    
+                    configuredatabase(true);
                 }
             }
             //
             //Create table(s)
             //
             MySqlCommand set_table = new MySqlCommand("CREATE TABLE IF NOT EXISTS telemetrytable (Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, RecDate TEXT, RecTime TEXT, Longitude TEXT, Latitude TEXT, Altitude DECIMAL, Velocity DECIMAL);", svt_telemetry);
-            set_table.ExecuteNonQuery();
+            try
+            {
+                set_table.ExecuteNonQuery();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                ;
+            }
         }
         //Gets the last 5 rows from the database and averages them
         public double get_average(string fieldname, int numRecords)
@@ -97,17 +116,24 @@ namespace SQL
 
         public void update_database(string date, string utc, string longitude, string latitude, string velocity, string altitude)
         {
-            MySqlCommand add_to_database = new MySqlCommand();
-            add_to_database.Connection = svt_telemetry;
-            add_to_database.Parameters.AddWithValue("@date", date); //Stackoverflow insisted this be the way this is done. I think it prevents SQL injections. Not that we should ever have to be concerned about those.
-            add_to_database.Parameters.AddWithValue("@utc", utc);
-            add_to_database.Parameters.AddWithValue("@longitude", longitude);
-            add_to_database.Parameters.AddWithValue("@latitude", latitude);
-            add_to_database.Parameters.AddWithValue("@altitude", altitude);
-            add_to_database.Parameters.AddWithValue("@velocity", velocity);
-            //If Structure Changes remember to change the CREATE TABLE command in the constructer and delete the database...
-            add_to_database.CommandText = "INSERT INTO telemetrytable(RecDate,RecTime,Longitude,Latitude,Altitude,Velocity) VALUES (@date,@utc,@longitude,@latitude,@altitude,@velocity)";
-            add_to_database.ExecuteNonQuery();
+            try
+            {
+                MySqlCommand add_to_database = new MySqlCommand();
+                add_to_database.Connection = svt_telemetry;
+                add_to_database.Parameters.AddWithValue("@date", date); //Stackoverflow insisted this be the way this is done. I think it prevents SQL injections. Not that we should ever have to be concerned about those.
+                add_to_database.Parameters.AddWithValue("@utc", utc);
+                add_to_database.Parameters.AddWithValue("@longitude", longitude);
+                add_to_database.Parameters.AddWithValue("@latitude", latitude);
+                add_to_database.Parameters.AddWithValue("@altitude", altitude);
+                add_to_database.Parameters.AddWithValue("@velocity", velocity);
+                //If Structure Changes remember to change the CREATE TABLE command in the constructer and delete the database...
+                add_to_database.CommandText = "INSERT INTO telemetrytable(RecDate,RecTime,Longitude,Latitude,Altitude,Velocity) VALUES (@date,@utc,@longitude,@latitude,@altitude,@velocity)";
+                add_to_database.ExecuteNonQuery();
+            }
+            catch
+            {
+                configuredatabase(false);
+            }
         }
     }
 }
