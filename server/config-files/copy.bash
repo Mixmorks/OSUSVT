88fb2214f058f9928f7083800a8fbf183173bd36
Cody Ray Hoeft <crhoeft@gmail.com>
2014-05-31 17:35:43 -0700

    Removed Unnecessary Files

--- server/config-files/copy.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" &&
-echo $DIR
-rsync -av $DIR/../solarcar-html-source/ $DIR/../solarcar-html/ &&
-echo "Directory Updated..."
