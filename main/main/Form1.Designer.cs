32911d5152d74b65690af88ec2eadeb6dbd7fd4e
Uli Bartels <bartelja@onid.oregonstate.edu>
2014-06-02 17:34:42 -0700

    Playing with the UI

--- main/main/Form1.Designer.cs
+++ main/main/Form1.Designer.cs
@@ -53,9 +53,9 @@
             this.left_blnk.BackColor = System.Drawing.Color.Transparent;
             this.left_blnk.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("left_blnk.BackgroundImage")));
             this.left_blnk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.left_blnk.Location = new System.Drawing.Point(2, 105);
+            this.left_blnk.Location = new System.Drawing.Point(0, 191);
             this.left_blnk.Name = "left_blnk";
-            this.left_blnk.Size = new System.Drawing.Size(127, 217);
+            this.left_blnk.Size = new System.Drawing.Size(184, 337);
             this.left_blnk.TabIndex = 1;
             this.left_blnk.TabStop = false;
             this.left_blnk.Click += new System.EventHandler(this.button1_Click);
@@ -66,9 +66,9 @@
             this.right_blnk.BackColor = System.Drawing.Color.Transparent;
             this.right_blnk.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("right_blnk.BackgroundImage")));
             this.right_blnk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.right_blnk.Location = new System.Drawing.Point(653, 105);
+            this.right_blnk.Location = new System.Drawing.Point(1096, 191);
             this.right_blnk.Name = "right_blnk";
-            this.right_blnk.Size = new System.Drawing.Size(127, 217);
+            this.right_blnk.Size = new System.Drawing.Size(184, 337);
             this.right_blnk.TabIndex = 15;
             this.right_blnk.TabStop = false;
             this.right_blnk.Click += new System.EventHandler(this.button2_Click);
@@ -80,9 +80,9 @@
             this.emergency_lights.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("emergency_lights.BackgroundImage")));
             this.emergency_lights.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.emergency_lights.InitialImage = null;
-            this.emergency_lights.Location = new System.Drawing.Point(653, 405);
+            this.emergency_lights.Location = new System.Drawing.Point(1096, 673);
             this.emergency_lights.Name = "emergency_lights";
-            this.emergency_lights.Size = new System.Drawing.Size(127, 83);
+            this.emergency_lights.Size = new System.Drawing.Size(184, 123);
             this.emergency_lights.TabIndex = 16;
             this.emergency_lights.TabStop = false;
             this.emergency_lights.Text = "button3";
@@ -94,9 +94,9 @@
             this.horn.BackColor = System.Drawing.Color.Transparent;
             this.horn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("horn.BackgroundImage")));
             this.horn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.horn.Location = new System.Drawing.Point(0, 405);
+            this.horn.Location = new System.Drawing.Point(0, 534);
             this.horn.Name = "horn";
-            this.horn.Size = new System.Drawing.Size(129, 82);
+            this.horn.Size = new System.Drawing.Size(184, 133);
             this.horn.TabIndex = 17;
             this.horn.TabStop = false;
             this.horn.Text = "button4";
@@ -105,9 +105,9 @@
             // 
             this.camera_window.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.camera_window.Location = new System.Drawing.Point(128, 105);
+            this.camera_window.Location = new System.Drawing.Point(190, 191);
             this.camera_window.Name = "camera_window";
-            this.camera_window.Size = new System.Drawing.Size(525, 382);
+            this.camera_window.Size = new System.Drawing.Size(900, 553);
             this.camera_window.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.camera_window.TabIndex = 13;
             this.camera_window.TabStop = false;
@@ -116,11 +116,11 @@
             // 
             this.velocity.AutoSize = true;
             this.velocity.BackColor = System.Drawing.Color.Transparent;
-            this.velocity.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.velocity.Font = new System.Drawing.Font("Microsoft Sans Serif", 180F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.velocity.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.velocity.Location = new System.Drawing.Point(-24, -24);
+            this.velocity.Location = new System.Drawing.Point(-46, -37);
             this.velocity.Name = "velocity";
-            this.velocity.Size = new System.Drawing.Size(213, 152);
+            this.velocity.Size = new System.Drawing.Size(380, 272);
             this.velocity.TabIndex = 19;
             this.velocity.Text = "88";
             // 
@@ -159,9 +159,9 @@
             this.lights.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.lights.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("lights.BackgroundImage")));
             this.lights.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.lights.Location = new System.Drawing.Point(653, 322);
+            this.lights.Location = new System.Drawing.Point(1096, 534);
             this.lights.Name = "lights";
-            this.lights.Size = new System.Drawing.Size(127, 83);
+            this.lights.Size = new System.Drawing.Size(184, 133);
             this.lights.TabIndex = 23;
             this.lights.TabStop = false;
             this.lights.Click += new System.EventHandler(this.lights_Click);
@@ -171,7 +171,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(780, 486);
+            this.ClientSize = new System.Drawing.Size(1280, 800);
             this.Controls.Add(this.lights);
             this.Controls.Add(this.label_soc);
             this.Controls.Add(this.left_blnk);
@@ -182,10 +182,9 @@
             this.Controls.Add(this.label_power_in);
             this.Controls.Add(this.label_power_out);
             this.Controls.Add(this.velocity);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "mainForm";
             this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.left_blnk)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.right_blnk)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.emergency_lights)).EndInit();
